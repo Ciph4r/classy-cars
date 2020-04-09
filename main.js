cdb098b927e0d4000d857924a340ade2682daac4
David Lau <David.lau@codeimmersives.com>
2020-04-09 15:40:55 -0400

    done

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 class Vehicle {
+  constructor(name , wheelCount,spd){
+    this.name = name
+    this.wheelCount = wheelCount
+    this.maxSpeed = spd
+    this.speed = 0
+  }
+  accelerate(){
+    if (this.name === 'Ferrari'){
+      this.speed + this.maxSpeed/2 > this.maxSpeed ? 
+      this.speed = this.maxSpeed :
+      this.speed += this.maxSpeed/2
+    }else{
+    this.speed + this.maxSpeed/3 > this.maxSpeed ? 
+    this.speed = this.maxSpeed :
+    this.speed += this.maxSpeed/3
+    }
+  }
 }
 
-class Car {
+class Car extends Vehicle{
+  constructor(name,spd){
+  super(name ,4,spd)
+  }
+  drive(){
+    return this.name === 'Ferrari'? "Vroom. I'm a Ferrari!" :
+     "Vroom. "
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car{
+  constructor(){
+    super('Ferrari',180)
+  }
 }
 
 
